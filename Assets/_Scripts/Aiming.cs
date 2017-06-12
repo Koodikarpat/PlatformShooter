a9ee653bd1bb2e786f0eaed29efeef7e32b03a63
tommilamsa <lamsatommi@gmail.com>
2017-06-12 15:01:18 +0300

    korjauksia tähtäykseen

---
+++ Assets/_Scripts/Aiming.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aiming : MonoBehaviour
+{
+    public BoxHP boxihp;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CheckHits()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        boxihp = GameObject.Find("Cube").GetComponent<BoxHP>();
+        if (Physics.Raycast(transform.position, forward, 100, LayerMask.GetMask("Player")))
+        {
+
+            print("Hit!");
+            boxihp.TakeDamage();
+
+        }
+    }
+}
