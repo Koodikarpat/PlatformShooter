9d828abd13e9034cb991d3201481f534549f99ea
Nelli Kemi <nelli.kemi@gmail.com>
2017-07-14 15:13:37 +0300

    Sniper bug fixes

--- Assets/_Scripts/Damage2.cs
+++ Assets/_Scripts/Damage2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Damage : MonoBehaviour {
+public class Damage2 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
